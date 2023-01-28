672201d2e9d9b53629eb389a979842ec81e62bf9
Kaio Silveira <kaionpsa@gmail.com>
2023-01-28 15:34:41 -0300

    disabled middleware to get users

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ import { verifyToken } from "../middleware/auth.js"
 
 const router = express.Router()
 
-router.get("/:id", verifyToken, getUser)
+router.get("/:id", getUser)
 router.get("/:id/friends", verifyToken, getUserFriends)
 
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend)
